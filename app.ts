f002fac441c7dc7ae8c895a7d8f9163da877ed1b
Greninja49 <hadiskhanofficial@gmail.com>
2025-07-11 00:59:04 +0530

    added env

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
 import router from "./router/app.route";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -8,7 +10,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/", router);
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
